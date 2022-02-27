c06542906107b3c420ffbb42d7e7d4097a6bff56
Johnny Rocketfingers <developer94@gmail.com>
2022-02-27 14:38:04 +0100

    Setup the npm package

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
 	//   "<rootDir>/src"
 	// ],
 	"testMatch": [
-	  "**/__tests__/**/*.+(ts|tsx|js)",
-	  "**/?(*.)+(spec|test).+(ts|tsx|js)"
+	  "**/__tests__/**/*.+(ts|tsx)",
+	  "**/?(*.)+(spec|test).+(ts|tsx)"
 	],
 	"moduleNameMapper": {
 		"@app/(.*)": "<rootDir>/src/$1",
